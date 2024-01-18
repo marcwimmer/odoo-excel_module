0253dccee91aa605f3948b93a6d58bf33718761a
Marc Wimmer <marc@itewimmer.de>
2024-01-18 11:40:07 +0100

    Update controller.py

--- excel_module/excel_module/controller.py
+++ excel_module/excel_module/controller.py
@@ -2,17 +2,21 @@ import base64
 from odoo import http
 from odoo.http import request
 from odoo.http import content_disposition
+from odoo.tools.safe_eval import safe_eval as eval
+from datetime import time
 
 class ExcelReportController(http.Controller):
 
     @http.route('/excel_report/<report_name>', auth='user', type="http")
-    def get_excel_report(self, report_name, model, res_id, **post):
+    def get_excel_report(self, report_name, model=None, res_id=None, **post):
         try:
             res_id = int(res_id)
         except Exception:
-            if ',' in res_id:
+            if ',' in (res_id or ""):
                 res_id = res_id.split(',')
                 res_id = [int(x) for x in res_id]
+            else:
+                res_id = None
 
         report = request.env['ir.actions.report'].sudo().search([
             ('report_name', '=', report_name),
@@ -20,24 +24,14 @@ class ExcelReportController(http.Controller):
         if report.report_type != 'excel':
             return
 
-        if not model:
-            model = report.model
+        content = report.render_excel(res_id)[0]
 
-        records = request.env[model].browse(res_id)
-        report_object = request.env['report.' + report_name]
-        if getattr(report_object, 'excel_as_binary', False):
-            content = report_object.excel_as_binary(records)
-        else:
-            RES = report_object.excel(records)
-            if isinstance(RES, dict):
-                RES = (RES, {})
-            if len(RES) != 2:
-                raise Exception("Please return (data, options) as tuple.")
-            data, options = RES
-            assert isinstance(data, dict), "Returned Data must be of type dict!"
-            content = request.env['excel.maker'].create_excel(request.env, data, **options)
+        object = request.env[report.model].browse(res_id)
 
-        name = report.print_report_name or report_name + '.xlsx'
+        name = report_name + '.xlsx'
+        if report.print_report_name:
+            name = eval(report.print_report_name, {'object': object, 'time': time})
+
         return http.request.make_response(content, [
             ('Content-Type', 'application/octet-stream; charset=binary'),
             ('Content-Disposition', content_disposition(name))
